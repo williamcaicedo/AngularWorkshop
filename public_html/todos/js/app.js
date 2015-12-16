7d304eded9046d318e95c330301daeea7b9b75a7
William <proyectos@nativapps.com>
2015-12-16 16:58:07 -0500

    localStorage support added

--- public_html/todos/js/app.js
+++ public_html/todos/js/app.js
@@ -9,16 +9,34 @@ lodash.factory('_', function(){
         return window._;
 });
 
+var serviceModule = angular.module('workshopAppServices', []);
+serviceModule.factory('persistenceService', ['_', function(_) {
+    return {
+        saveTask: function(task) {
+            var tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+            tasks.push(task);
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+        },
+        getAllTasks: function() {
+            var tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+            return tasks; 
+        }
+    }    
+}]);
+
+
 var controllerModule = angular.module('workshopAppControllers',[]);
 
-controllerModule.controller('homeController',['$scope', 'rfc4122', '_',function($scope, idGen, _) {
+controllerModule.controller('homeController',['$scope', 'rfc4122', '_', 
+    'persistenceService',function($scope, idGen, _, persistenceService) {
         $scope.task = {};
-        $scope.tasks = [];
+        $scope.tasks = persistenceService.getAllTasks();
         $scope.finishedTasks = [];
         
         $scope.createTask = function() {
             $scope.task.id = idGen.v4();
             $scope.tasks.push($scope.task);
+            persistenceService.saveTask($scope.task);
             $scope.task = {};
         };
         $scope.finishTask = function(task) {
@@ -33,6 +51,6 @@ controllerModule.controller('homeController',['$scope', 'rfc4122', '_',function(
 }]);
 
 
-angular.module('workshopApp',['workshopAppControllers','uuid', 'lodash']);
+angular.module('workshopApp',['workshopAppControllers', 'workshopAppServices', 'uuid', 'lodash']);
 
 
